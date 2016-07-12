0d123e08893013eb78723d517f2b4576bd5d2e0f
Dorothy Jung <jungy@berkeley.edu>
2016-07-11 22:48:55 -0700

    set up landing page templates

--- index.js
+++ index.js
@@ -13,6 +13,54 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/lessons', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/lessons/energy', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/lessons/kinetics', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/lessons/keq', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/lessons/ph', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/lessons/echem', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/prep', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/prep/categorized', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/prep/qanda', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/reading/', function(request, response) {
+  response.render('pages/energy');
+});
+
+app.get('/topics', function(request, response) {
+  response.render('pages/energy');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
